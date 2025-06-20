12e823b9eb95c4af54106e43dc82159988ca2d53
iertis <kanopus952@gmail.com>
2025-06-20 15:35:53 +0300

    fix

--- Content.Shared/_Sunrise/Restrict/RestrictByUserTagComponent.cs
+++ Content.Shared/_Sunrise/Restrict/RestrictByUserTagComponent.cs
@@ -1,4 +1,4 @@
-﻿using Content.Shared.Starlight.Antags.Abductor;
+﻿using Content.Shared._Sunrise.Antags.Abductor;
 using Content.Shared.Tag;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
