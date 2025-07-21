7f93c01d78c01ed75318d71cffbc4dd524f4e795
iertis <kanopus952@gmail.com>
2025-07-21 20:23:53 +0300

    final fixes

--- Content.Shared/_Sunrise/Felinid/SharedFelinidSystem.cs
+++ Content.Shared/_Sunrise/Felinid/SharedFelinidSystem.cs
@@ -5,6 +5,7 @@ using Content.Shared.Interaction.Events;
 using Content.Shared.Inventory.Events;
 using Content.Shared.Item;
 using Content.Shared.Movement.Pulling.Events;
+using Content.Shared.Popups;
 using Content.Shared.Throwing;
 using Robust.Shared.Containers;
 
@@ -14,6 +15,7 @@ public abstract class SharedFelinidSystem : EntitySystem
 {
     [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -69,7 +71,6 @@ public abstract class SharedFelinidSystem : EntitySystem
         if (HasComp<FelinidComponent>(args.Item) || component.InContainer)
             args.Cancel();
     }
-
     private void OnGettingPickupAttempt(EntityUid uid, FelinidComponent component, ref GettingPickedUpAttemptEvent args)
     {
         args.Cancel();
@@ -100,6 +101,14 @@ public abstract class SharedFelinidSystem : EntitySystem
 
         if (!_hands.TryGetEmptyHand(args.Args.User, out var emptyHand, hands))
             return;
+
+        if (TryComp<MultiHandedItemComponent>(args.Args.Target.Value, out var multiHanded)
+            && hands.CountFreeHands() != multiHanded!.HandsNeeded)
+        {
+            _popup.PopupPredictedCursor(Loc.GetString("multi-handed-item-pick-up-fail",
+                ("number", multiHanded.HandsNeeded - 1), ("item", ent.Owner)), args.Args.User);
+            return;
+        }
 
         _hands.TryPickup(args.Args.User, args.Args.Target.Value, emptyHand, checkActionBlocker: false, handsComp: hands);
         args.Handled = true;
