c6221408663409feb88714615050efd6eae7eda3
iertis <kanopus952@gmail.com>
2026-01-06 19:54:40 +0300

    еще фикс биокода

--- Content.Shared/_Sunrise/FactionClothingBlockerSystem/FactionClothingBlockerSystem.cs
+++ Content.Shared/_Sunrise/FactionClothingBlockerSystem/FactionClothingBlockerSystem.cs
@@ -1,11 +1,14 @@
 using Content.Shared._Sunrise.Biocode;
 using Content.Shared.Inventory.Events;
+using Robust.Shared.Network;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._Sunrise.FactionClothingBlockerSystem;
 
 public sealed class FactionClothingBlockerSystem : EntitySystem
 {
     [Dependency] private readonly BiocodeSystem _biocodeSystem = default!;
+    [Dependency] private readonly INetManager _net = default!;
 
     public override void Initialize()
     {
@@ -16,6 +19,9 @@ public sealed class FactionClothingBlockerSystem : EntitySystem
 
     private async void OnEquippedAttempt(EntityUid uid, FactionClothingBlockerComponent component, BeingEquippedAttemptEvent args)
     {
+        if (_net.IsClient)
+            return;
+
         if (TryComp<BiocodeComponent>(args.Equipment, out var biocodedComponent))
         {
             if (!_biocodeSystem.CanUse(args.EquipTarget, biocodedComponent.Factions))
