654a04da95f1f48603dfda43bdffe049d5ff6816
Absotively <jen@jenpollock.ca>
2026-01-26 11:18:29 -0700

    Move character preview handling into a specialized control (#41252)
    * Move character preview handling into a specialized control
    
    Co-authored-by: Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
    
    * Restore job name that I accidentally removed from character picker buttons
    
    * Just resolve dependencies the standard way
    
    ---------
    
    Co-authored-by: Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
    Co-authored-by: Janet Blackquill <uhhadd@gmail.com>

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -77,11 +77,6 @@ namespace Content.Client.Lobby.UI
         public event Action? Save;
 
         /// <summary>
-        /// Entity used for the profile editor preview
-        /// </summary>
-        public EntityUid PreviewDummy;
-
-        /// <summary>
         /// Temporary override of their selected job, used to preview roles.
         /// </summary>
         public JobPrototype? JobOverride;
@@ -878,15 +873,10 @@ namespace Content.Client.Lobby.UI
         /// </remarks>
         private void ReloadPreview()
         {
-            _entManager.DeleteEntity(PreviewDummy);
-            PreviewDummy = EntityUid.Invalid;
-
-            if (Profile == null || !_prototypeManager.HasIndex(Profile.Species))
+            if (Profile == null)
                 return;
 
-            PreviewDummy = _controller.LoadProfileEntity(Profile, JobOverride, ShowClothes.Pressed);
-            SpriteView.SetEntity(PreviewDummy);
-            _entManager.System<MetaDataSystem>().SetEntityName(PreviewDummy, Profile.Name);
+            SpriteView.LoadPreview(Profile, JobOverride, ShowClothes.Pressed);
 
             // Check and set the dirty flag to enable the save/reset buttons as appropriate.
             SetDirty();
@@ -943,16 +933,15 @@ namespace Content.Client.Lobby.UI
             }
         }
 
-
         /// <summary>
         /// A slim reload that only updates the entity itself and not any of the job entities, etc.
         /// </summary>
         private void ReloadProfilePreview()
         {
-            if (Profile == null || !_entManager.EntityExists(PreviewDummy))
+            if (Profile == null)
                 return;
 
-            _entManager.System<SharedVisualBodySystem>().ApplyProfileTo(PreviewDummy, Profile);
+            SpriteView.ReloadProfilePreview(Profile);
 
             // Check and set the dirty flag to enable the save/reset buttons as appropriate.
             SetDirty();
@@ -1376,13 +1365,6 @@ namespace Content.Client.Lobby.UI
             ReloadPreview();
         }
 
-        protected override void ExitedTree()
-        {
-            base.ExitedTree();
-            _entManager.DeleteEntity(PreviewDummy);
-            PreviewDummy = EntityUid.Invalid;
-        }
-
         private void SetAge(int newAge)
         {
             Profile = Profile?.WithAge(newAge);
@@ -1514,7 +1496,7 @@ namespace Content.Client.Lobby.UI
             if (!IsDirty)
                 return;
 
-            _entManager.System<MetaDataSystem>().SetEntityName(PreviewDummy, newName);
+            SpriteView.SetName(newName);
         }
 
         private void SetSpawnPriority(SpawnPriorityPreference newSpawnPriority)
@@ -1891,7 +1873,7 @@ namespace Content.Client.Lobby.UI
 
             // I tried disabling the button but it looks sorta goofy as it only takes a frame or two to save
             _imaging = true;
-            await _entManager.System<ContentSpriteSystem>().Export(PreviewDummy, dir, includeId: false);
+            await _entManager.System<ContentSpriteSystem>().Export(SpriteView.PreviewDummy, dir, includeId: false);
             _imaging = false;
         }
 
---
+++ Content.Client/Lobby/UI/ProfileEditorControls/ProfilePreviewSpriteView.cs
@@ -0,0 +1,86 @@
+using Content.Shared.Preferences;
+using Content.Shared.Roles;
+using Content.Sunrise.Interfaces.Shared;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.Lobby.UI.ProfileEditorControls;
+
+public sealed partial class ProfilePreviewSpriteView : SpriteView
+{
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly ISharedPlayerManager _playerManager = default!;
+
+    private ISharedSponsorsManager? _sponsorsManager;
+
+    /// <summary>
+    /// Entity used for the profile editor preview
+    /// </summary>
+    public EntityUid PreviewDummy;
+
+    public ProfilePreviewSpriteView()
+    {
+        IoCManager.InjectDependencies(this);
+        IoCManager.Instance!.TryResolveType(out _sponsorsManager);
+    }
+
+    /// <summary>
+    /// Reloads the entire dummy entity for preview.
+    /// </summary>
+    /// <remarks>
+    /// This is expensive so not recommended to run if you have a slider.
+    /// </remarks>
+    public void LoadPreview(ICharacterProfile profile, JobPrototype? jobOverride = null, bool showClothes = true)
+    {
+        EntMan.DeleteEntity(PreviewDummy);
+        PreviewDummy = EntityUid.Invalid;
+
+        switch (profile)
+        {
+            case HumanoidCharacterProfile humanoid:
+                LoadHumanoidEntity(humanoid, jobOverride, showClothes);
+                break;
+            default:
+                throw new ArgumentException("Only humanoid profiles are implemented in ProfilePreviewSpriteView");
+        }
+
+        SetEntity(PreviewDummy);
+        SetName(profile.Name);
+    }
+
+    /// <summary>
+    /// Sets the preview entity's name without reloading anything else.
+    /// </summary>
+    public void SetName(string newName)
+    {
+        EntMan.System<MetaDataSystem>().SetEntityName(PreviewDummy, newName);
+    }
+
+    /// <summary>
+    /// A slim reload that only updates the entity itself and not any of the job entities, etc.
+    /// </summary>
+    public void ReloadProfilePreview(ICharacterProfile profile)
+    {
+        switch (profile)
+        {
+            case HumanoidCharacterProfile humanoid:
+                ReloadHumanoidEntity(humanoid);
+                break;
+            default:
+                throw new ArgumentException("Only humanoid profiles are implemented in ProfilePreviewSpriteView");
+        }
+    }
+
+    public void ClearPreview()
+    {
+        EntMan.DeleteEntity(PreviewDummy);
+        PreviewDummy = EntityUid.Invalid;
+    }
+
+    protected override void ExitedTree()
+    {
+        base.ExitedTree();
+        ClearPreview();
+    }
+}
