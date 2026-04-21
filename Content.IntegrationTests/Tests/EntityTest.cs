8b5a20a2964b1d3049f2e5f268206d029b9e367b
iertis <kanopus952@gmail.com>
2026-04-21 15:20:29 +0300

    fixes

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -143,7 +143,6 @@ namespace Content.IntegrationTests.Tests
         ///     all components on every entity.
         /// </summary>
         [Test]
-        [Ignore("test")]
         public async Task SpawnAndDirtyAllEntities()
         {
             // This test dirties the pair as it simply deletes ALL entities when done. Overhead of restarting the round
